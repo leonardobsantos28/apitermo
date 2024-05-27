5e8f231453ccb04050e02ad3dea0713431cb2d03
leonardobsantos28 <84942205+leonardobsantos28@users.noreply.github.com>
2024-05-27 13:31:20 -0300

    apitermo3 inicio

--- src/main/java/com/wlsoftware/apitermo/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.wlsoftware.apitermo.exceptions;
-
-public class BusinessException extends RuntimeException{
-
-    public BusinessException(String message){
-        super(message);
-    }
-
-}
